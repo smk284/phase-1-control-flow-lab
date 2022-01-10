8d6524ada5a30e89fdd6bb841d4009a8c8bb38c7
Shaik Kamil <shaikm.kamil@gmail.com>
2022-01-10 13:48:42 -0500

    commit tree 574db2a89ed15e4d690ecada7aef8fd93e73efb2

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+    if (ride <= '400') {
+      return "This one is on me!"
+    } else if (ride < '2500') {
+      return 'I will gladly take your thirty bucks.'
+    } else if (ride > '2500') {
+      return 'No can do.'}
+    }
+
+function ternaryCheckCity(city){
+  return ternaryCheckCity = city ==='NYC' ? 'Ok, sounds good.' : 'No go.' // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
+  switch(amount) {
+    case 'generous' :
+      return 'Thank you so much.';
+      case 'not as generous' :
+        return 'Thank you.';
+        default: return 'Bye.';
+  }
   // Write your code here!
+
 }
